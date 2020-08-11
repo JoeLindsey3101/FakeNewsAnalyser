3c09e9362be7936843b627fffbf8180019706276
JoeLindsey3101 <Joe71644@gmail.com>
2020-08-11 23:38:39 +0100

    Added New Folder.

---
+++ FakeNewsAnalyser/FakeNewsAnalyser/HelpForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace FakeNewsAnalyser
+{
+    partial class HelpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.AnalyserBtn = new System.Windows.Forms.Button();
+            this.HomeBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.Location = new System.Drawing.Point(551, 119);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(96, 37);
+            this.UpdateBtn.TabIndex = 11;
+            this.UpdateBtn.Text = "Update Database";
+            this.UpdateBtn.UseVisualStyleBackColor = true;
+            // 
+            // AnalyserBtn
+            // 
+            this.AnalyserBtn.Location = new System.Drawing.Point(325, 119);
+            this.AnalyserBtn.Name = "AnalyserBtn";
+            this.AnalyserBtn.Size = new System.Drawing.Size(96, 37);
+            this.AnalyserBtn.TabIndex = 10;
+            this.AnalyserBtn.Text = "Analyser";
+            this.AnalyserBtn.UseVisualStyleBackColor = true;
+            this.AnalyserBtn.Click += new System.EventHandler(this.AnalyserBtn_Click);
+            // 
+            // HomeBtn
+            // 
+            this.HomeBtn.Location = new System.Drawing.Point(102, 119);
+            this.HomeBtn.Name = "HomeBtn";
+            this.HomeBtn.Size = new System.Drawing.Size(96, 37);
+            this.HomeBtn.TabIndex = 9;
+            this.HomeBtn.Text = "Home";
+            this.HomeBtn.UseVisualStyleBackColor = true;
+            this.HomeBtn.Click += new System.EventHandler(this.HomeBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(415, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Help identify Fake News";
+            // 
+            // HelpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1042, 762);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.AnalyserBtn);
+            this.Controls.Add(this.HomeBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "HelpForm";
+            this.Text = "HelpForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.Button AnalyserBtn;
+        private System.Windows.Forms.Button HomeBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
